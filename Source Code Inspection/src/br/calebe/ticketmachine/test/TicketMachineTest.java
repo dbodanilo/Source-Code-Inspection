d72956b80740bc793dd22aa34e457d3ccaaff537
Danilo Bizarria <unifdaoliveira@fei.edu.br>
2022-04-09 19:46:56 -0300

    add getTrocoTest
    also add timeout rule to catch Iterators in infinite loops

--- Source Code Inspection/src/br/calebe/ticketmachine/test/TicketMachineTest.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/test/TicketMachineTest.java
@@ -1,10 +1,21 @@
 package br.calebe.ticketmachine.test;
 
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
+
+import br.calebe.ticketmachine.core.PapelMoeda;
 import br.calebe.ticketmachine.core.TicketMachine;
 
 public class TicketMachineTest {
+    
+    // prevents Iterator<> tests from getting stuck in an infinite loop
+    @Rule
+    public Timeout globalTimeout= new Timeout(100, TimeUnit.MILLISECONDS);
     
     @Test
     public void inserirValorTest(){
@@ -19,6 +30,22 @@ public class TicketMachineTest {
             e.printStackTrace();
             Assert.fail();
         }
+    }
+    
+    @Test
+    public void getTrocoTest() {
+        int valorBilhete = 5;
+        TicketMachine tM = new TicketMachine(valorBilhete);
+        int saldoOriginal = tM.getSaldo();
+        Iterator<PapelMoeda> troco = tM.getTroco();
+
+        int totalTroco = 0;
+        while (troco.hasNext()) {
+            PapelMoeda papel = troco.next();
+            totalTroco += papel.getValor() * papel.getQuantidade();
+        }
+            
+        Assert.assertEquals(totalTroco, saldoOriginal);
     }
 
     @Test
