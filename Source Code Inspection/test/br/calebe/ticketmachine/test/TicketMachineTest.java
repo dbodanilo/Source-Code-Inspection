ad01cb2b32364e253074afdbece36c605387cb62
Danilo Bizarria <unifdaoliveira@fei.edu.br>
2022-04-15 19:36:28 -0300

    move tests from src to test folder
    also add testGetSaldo

--- Source Code Inspection/test/br/calebe/ticketmachine/test/TicketMachineTest.java
+++ Source Code Inspection/test/br/calebe/ticketmachine/test/TicketMachineTest.java
@@ -11,14 +11,22 @@ import org.junit.rules.Timeout;
 import br.calebe.ticketmachine.core.PapelMoeda;
 import br.calebe.ticketmachine.core.TicketMachine;
 
+/**
+ *
+ * @author danil
+ */
 public class TicketMachineTest {
     
     // prevents Iterator<> tests from getting stuck in an infinite loop
     @Rule
     public Timeout globalTimeout= new Timeout(100, TimeUnit.MILLISECONDS);
-    
+
+    /**
+     * Test of inserir method, of class TicketMachine.
+     */    
     @Test
-    public void inserirValorTest(){
+    public void testInserir() throws Exception {
+        System.out.println("inserir");
         int valorBilhete = 5;
         int quantiaInserida = 10;
         TicketMachine tM = new TicketMachine(valorBilhete);
@@ -31,12 +39,30 @@ public class TicketMachineTest {
             Assert.fail();
         }
     }
-    
+
+    /**
+     * Test of getSaldo method, of class TicketMachine.
+     */
     @Test
-    public void getTrocoTest() {
+    public void testGetSaldo() {
+        System.out.println("getSaldo");
         int valorBilhete = 5;
         TicketMachine tM = new TicketMachine(valorBilhete);
-        int saldoOriginal = tM.getSaldo();
+
+        int expResult = 0;
+        int result = tM.getSaldo();
+        Assert.assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getTroco method, of class TicketMachine.
+     */
+    @Test
+    public void testGetTroco() {
+        System.out.println("getTroco");
+        int valorBilhete = 5;
+        TicketMachine tM = new TicketMachine(valorBilhete);
+        int saldoOriginal = 0;
         Iterator<PapelMoeda> troco = tM.getTroco();
 
         int totalTroco = 0;
@@ -48,21 +74,24 @@ public class TicketMachineTest {
         Assert.assertEquals(totalTroco, saldoOriginal);
     }
 
+    /**
+     * Test of imprimir method, of class TicketMachine.
+     */
     @Test
-    public void imprimirTest() {
+    public void testImprimir() throws Exception {
+        System.out.println("imprimir");
         int valorBilhete = 5;
-        int saldo = 5;
-        int result = "*****************\n*** R$ " + saldo + ",00 ****\n*****************\n";
+        int saldo = 0;
+        String expResult = "*****************\n*** R$ " + saldo + ",00 ****\n*****************\n";
         
         TicketMachine tM = new TicketMachine(valorBilhete);
         try {
-            tM.inserir(quantiaInserida);
+            String result = tM.imprimir();
             
-            Assert.assertEquals(result, tM.imprimir());
+            Assert.assertEquals(expResult, result);
         } catch (Exception e) {
             e.printStackTrace();
             Assert.fail();
         }
     }
-
 }
